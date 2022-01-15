78575b47ef2c0957932604fd13b171d0f1791a81
Noor-Masrur <masrur1707036@stud.kuet.ac.bd>
2022-01-15 16:48:15 +0600

    new repository

---
+++ app/database/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$host ='localhost';
+$user = 'root';
+$pass = '';
+$db_name = 'bja';
+
+$conn =  new MySQLi($host,$user,$pass,$db_name);
+
+if( $conn->connect_error){
+    die('Database connection error'.$conn->connect_error);
+}
+else{
+    echo "Successful";
+}
+
+?>
